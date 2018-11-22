dd9d0901667755dd2238b3a5d2e72cad71b77c0a
superman285 <superman285@126.com>
2018-11-22 22:12:07 +0800

    众筹电影erc20相关合约更新~ update on 2018.11.22

---
+++ BusinessDemo/CrowdFunding-Movie/tokenModule/kccToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.25;
+
+contract kccToken {
+
+    //定义名称
+
+    //定义symbol，例如eth
+
+    //定义owner，基金会|管理者
+
+    //定义总发行量
+
+    //定义余额，账户=>余额
+
+    //授权余额，两重map，address=>(address=>uint) owner,spender,value
+
+
+    //构造函数，发行总量，管理者fundation，管理者持有token比例(用余额) 20%
+
+
+}
