befe2c5a185b14b61015ba2b13eda5d3d8713186
superman285 <superman285@126.com>
2019-03-22 19:59:27 +0800

    修改一些bug 基础五子棋游戏合约over 接下来添加时间和奖励相关 update on 2019.3.22

--- TicTacToe/TicTacToe.sol
+++ TicTacToe/TicTacToe.sol
@@ -1,29 +1,34 @@
 pragma solidity >=0.5.0 <=0.6.0;
 pragma experimental ABIEncoderV2;
 
+import "./Console.sol";
+
 contract TicTacToe {
 
-    address payable public hostPlayer;
-    address payable public guestPlayer;
+    address public hostPlayer;
+    address public guestPlayer;
     address public activePlayer;
     address public victorPlayer;
 
     bool public gameRunning;
 
+    string public gameResult;
+
     uint8 public chessboardSize = 3;
-    string[3][3] chessboard;
+    string[3][3] public chessboard;
 
-    mapping(address => string) playerChess;
-    mapping(address => uint8) movesCount;
-
-    function getWholeBoard() public view returns (string[3][3] memory){
-        return chessboard;
-    }
-
+    //"O"(host) or "X"(guest)
+    mapping(address => string) public playerChess;
+    mapping(address => uint8) public movesCount;
+    
     constructor() payable public{
         hostPlayer = msg.sender;
         gameRunning = false;
         playerChess[hostPlayer] = "O";
+    }
+
+    function getWholeBoard() public view returns (string[3][3] memory){
+        return chessboard;
     }
 
     function joinGame() public {
@@ -33,26 +38,39 @@ contract TicTacToe {
         //启动游戏
         gameRunning = true;
         playerChess[guestPlayer] = "X";
-        randomStart();
+        if(randomStart()==hostPlayer){
+            activePlayer = hostPlayer;
+        }else if(randomStart()==guestPlayer){
+            activePlayer = guestPlayer;
+        }
     }
 
-    function randomStart() public {
+    //前端怎么判断谁先开始,处理成返回值吧?或者谁先开始由前端随机?
+    //若前端随机还需要传给智能合约设置activeplayer又需要用户等待时间 还是智能合约处理吧
+    function randomStart() private view returns(address) {
         uint rand = uint(keccak256(abi.encodePacked(now, hostPlayer, guestPlayer))) % 2;
         if (rand == 0) {
-            activePlayer = hostPlayer;
+            return hostPlayer;
         } else {
-            activePlayer = guestPlayer;
+            return guestPlayer;
         }
     }
 
     //重新启动，重置参数
     function restartGame() public {
-        require(msg.sender==hostPlayer,"only creator has the restart right");
+        require(msg.sender == hostPlayer, "only creator has the restart right");
         //游戏未运行状态且guestPlayer存在，即游戏有胜负后 才可重启。防止creator随意重启
-        require(!gameRunning&&guestPlayer!=address(0),"you can only restart after the game is over");
+        require(!gameRunning && guestPlayer != address(0), "you can only restart after the game is over");
+
+        //步数置空,要在重置guestPlayer之前
+        delete movesCount[hostPlayer];
+        delete movesCount[guestPlayer];
+        //客方棋子置空,要在重置guestPlayer之前
+        delete playerChess[guestPlayer];
+        
         //重置guestPlayer,hostPlayer永远不变
         delete guestPlayer;
-        //重置活动玩家
+        //重置活动玩家(等价于activePlayer=address(0);)
         delete activePlayer;
         //棋盘置空
         delete chessboard;
@@ -77,30 +95,42 @@ contract TicTacToe {
             movesCount[guestPlayer]++;
         }
 
-        settleStage(row,column);
-
-        if(gameRunning){
-            takeTurnStage();
+        //优化下，至少5步才需要结算胜负
+        if ((movesCount[hostPlayer] + movesCount[guestPlayer]) >= (chessboardSize * 2 - 1)) {
+            Console.log("settle", now);
+            settleStage(row, column);
         }
 
+        if (gameRunning) {
+            takeTurnStage();
+        }
     }
 
     function setVictor(address player) private {
         gameRunning = false;
+        delete activePlayer;
         victorPlayer = player;
+        if(player==hostPlayer){
+            gameResult="hostVictory";
+        }else{
+            gameResult="guestVictory";
+        }
         //emit event
         //发奖
     }
 
     function setTie() private {
         gameRunning = false;
+        delete activePlayer;
+        gameResult = "tie";
         //emit event
         //平局发奖
     }
 
-    //每步结算阶段
+    //结算阶段
     function settleStage(uint8 player_row, uint8 player_column) private {
         //玩家所在列 达到3个
+        Console.log("firstJudge", now);
         uint8 row_count = 0;
         for (uint8 row = 0; row < chessboardSize; row++) {
             if (keccak256(abi.encodePacked(chessboard[row][player_column])) == keccak256(abi.encodePacked(playerChess[activePlayer]))) {
@@ -115,6 +145,7 @@ contract TicTacToe {
             }
         }
 
+        Console.log("secondJudge", now);
         //玩家所在行达到3个
         uint8 column_count = 0;
         for (uint8 column = 0; column < chessboardSize; column++) {
@@ -130,55 +161,58 @@ contract TicTacToe {
             }
         }
 
+        Console.log("thirdJudge", now);
         //玩家不在上下左右四个位置 就判对角线
         if (keccak256(abi.encodePacked(chessboard[0][1])) != keccak256(abi.encodePacked(playerChess[activePlayer])) &&
-         keccak256(abi.encodePacked(chessboard[1][0])) != keccak256(abi.encodePacked(playerChess[activePlayer])) &&
-         keccak256(abi.encodePacked(chessboard[1][2])) != keccak256(abi.encodePacked(playerChess[activePlayer])) &&
-         keccak256(abi.encodePacked(chessboard[2][1])) != keccak256(abi.encodePacked(playerChess[activePlayer])) ) {
+        keccak256(abi.encodePacked(chessboard[1][0])) != keccak256(abi.encodePacked(playerChess[activePlayer])) &&
+        keccak256(abi.encodePacked(chessboard[1][2])) != keccak256(abi.encodePacked(playerChess[activePlayer])) &&
+        keccak256(abi.encodePacked(chessboard[2][1])) != keccak256(abi.encodePacked(playerChess[activePlayer]))) {
 
             uint8 diagonalCount = 0;
             for (uint8 i = 0; i < chessboardSize; i++) {
                 if (keccak256(abi.encodePacked(chessboard[i][i])) == keccak256(abi.encodePacked(playerChess[activePlayer]))) {
                     diagonalCount++;
-                    if(diagonalCount == 3) {
+                    if (diagonalCount == 3) {
                         //activePlayer is winner
                         setVictor(activePlayer);
                         return;
                     }
-                }else{
+                } else {
                     break;
                 }
             }
 
+
             uint8 anti_diagonalCount = 0;
             for (uint8 j = 0; j < chessboardSize; j++) {
-                if (keccak256(abi.encodePacked(chessboard[j][chessboardSize-j])) == keccak256(abi.encodePacked(playerChess[activePlayer]))) {
+                if (keccak256(abi.encodePacked(chessboard[j][chessboardSize - j - 1])) == keccak256(abi.encodePacked(playerChess[activePlayer]))) {
                     anti_diagonalCount++;
-                    if(anti_diagonalCount == 3) {
+                    if (anti_diagonalCount == 3) {
                         //activePlayer is winner
                         setVictor(activePlayer);
                         return;
                     }
-                }else{
+                } else {
                     break;
                 }
             }
         }
 
         //判平局
-        if((movesCount[hostPlayer]+movesCount[guestPlayer])==(chessboardSize**2)){
+        if ((movesCount[hostPlayer] + movesCount[guestPlayer]) == (chessboardSize ** 2)) {
             //平局tie
             setTie();
         }
     }
 
     //换轮次阶段
-    function takeTurnStage() public {
+    function takeTurnStage() private {
         if (activePlayer == hostPlayer) {
             activePlayer = guestPlayer;
         } else if (activePlayer == guestPlayer) {
             activePlayer = hostPlayer;
         }
     }
+
 
 }
